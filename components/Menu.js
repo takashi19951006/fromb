77152266016f6b97220a3f3c2aa69325f08fd3ff
fujikitakashi <57948203+fujikitakashi@users.noreply.github.com>
2024-02-21 22:30:37 +0900

    サイドバーデザイン

--- components/Menu.js
+++ components/Menu.js
@@ -38,16 +38,23 @@ export function Menu() {
     <>
       <div onClick={toggleMenu}>
         <MenuContainer isOpen={isOpen} className="fixed h-screen  w-[40%] lg:w-[30%]  z-10 bg-black bg-opacity-70 transition-right duration-500">
-          <ul className="flex flex-col justify-end items-center text-white text-3xl h-[100%]">
-            <li className="hover:bg-gray-500 h-[10%]"><Link href={"#about"}>about</Link></li>
-            <li className="hover:bg-gray-500 h-[10%]"><Link href={"#photo"}>photo</Link></li>
-            <li className="hover:bg-gray-500 h-[10%]"><Link href={"#video"}>video</Link></li>
-            <li className="hover:bg-gray-500 h-[10%]"><Link href={"#contact"}>contact</Link></li>
-            <li className="hover:bg-gray-500 h-[10%]"></li>
+          <ul className="flex flex-col justify-end items-center text-white text-3xl h-[100%] w-full">
+            {["about", "photo", "video", "contact"].map((section, index) => (
+            <li
+              key={index}
+              className={`hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-blue-500 hover:to-red-500 hover:scale-110 h-[10%] w-full flex justify-center transition-all duration-200`}
+            >
+              <Link href={`#${section}`}>{section}</Link>
+            </li>
+            ))}
+            <li className="h-[10%]"></li>
           </ul>
         </MenuContainer>
-        {isOpen?<IoMdClose className="fixed right-2 top-2 h-10 w-10 z-10 m-2 rounded-full text-white"/>:<IoMdMenu className='fixed right-2 top-2 h-10 w-10 z-10 m-2 rounded-full'/>
-}        
+        {
+        isOpen?
+        <IoMdClose className="fixed right-2 top-2 h-10 w-10 z-10 m-2 rounded-full text-white hover:scale-110 transition-all duration-250"/>:
+        <IoMdMenu className='fixed right-2 top-2 h-10 w-10 z-10 m-2 rounded-full'/>
+        }        
       </div>
     </>
   );
